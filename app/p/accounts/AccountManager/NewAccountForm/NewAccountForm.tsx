94ce7e620766d4f7a9d95b3c31a0f507c028e413
Joshua Silveous <JoshSilveous@gmail.com>
2024-09-05 16:10:03 -0400

    added New Account Form

---
+++ app/p/accounts/AccountManager/NewAccountForm/NewAccountForm.tsx
@@ -0,0 +1,138 @@
+import { JButton } from '@/components/JForm/JButton/JButton'
+import s from './NewAccountForm.module.scss'
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react'
+import { JInput } from '@/components/JForm/JInput/JInput'
+import JNumberAccounting from '@/components/JForm/JNumberAccounting/JNumberAccounting'
+import { insertAccount } from './insertAccount'
+
+interface Errors {
+	name: string
+	starting_amount: string
+	general: string
+}
+
+export function NewAccountForm({ afterSubmit }: { afterSubmit: () => void }) {
+	const [formData, setFormData] = useState({ name: '', starting_amount: '' })
+	const [errors, setErrors] = useState<Errors>({
+		name: '',
+		starting_amount: '',
+		general: '',
+	})
+	const [isSubmitting, setIsSubmitting] = useState(false)
+
+	async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault()
+
+		setErrors({
+			name: '',
+			starting_amount: '',
+			general: '',
+		})
+
+		let formValid = true
+
+		// check that "name" isn't blank
+		if (formData.name === '') {
+			setErrors((prev) => ({
+				...prev,
+				name: 'You must enter an account name',
+			}))
+			formValid = false
+		}
+
+		// check that "starting_amount" isn't blank
+		if (formData.starting_amount === '') {
+			setErrors((prev) => ({
+				...prev,
+				starting_amount: 'You must enter a starting balance',
+			}))
+			formValid = false
+		}
+
+		if (formValid) {
+			setIsSubmitting(true)
+
+			const res = await insertAccount(formData.name.trim(), formData.starting_amount)
+			if (res?.error !== undefined) {
+				setErrors((prev) => ({
+					...prev,
+					general: res.error,
+				}))
+			} else {
+				afterSubmit()
+			}
+		}
+	}
+
+	function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+		e.target.value = e.target.value.trimStart()
+
+		let { name, value } = e.target
+		if (name === 'starting_amount') {
+			value = parseFloat(value).toFixed(2)
+		}
+		setFormData({
+			...formData,
+			[name]: value,
+		})
+		setErrors((prev) => ({ ...prev, [name]: '' }))
+	}
+
+	function handleBlur(e: ChangeEvent<HTMLInputElement>) {
+		let { name, value } = e.target
+		e.target.value = value.trim()
+		setFormData({
+			...formData,
+			[name]: value.trim(),
+		})
+	}
+
+	useEffect(() => {
+		console.log(formData)
+	}, [formData])
+
+	return (
+		<div>
+			<h1>Create New Account</h1>
+			<form className={s.form} onSubmit={handleSubmit} noValidate>
+				<div className={errors.name ? s.error : ''}>
+					<label htmlFor='name'>Account Name</label>
+					<JInput
+						id='name'
+						name='name'
+						type='text'
+						required
+						onChange={handleInputChange}
+						onBlur={handleBlur}
+						placeholder='e.x. Discover Credit'
+					/>
+					<div className={s.error_container}>
+						{errors.name && <div>{errors.name}</div>}
+					</div>
+				</div>
+				<div className={errors.starting_amount ? s.error : ''}>
+					<label htmlFor='starting_amount'>Starting Amount</label>
+					<JNumberAccounting
+						id='starting_amount'
+						name='starting_amount'
+						required
+						onChange={handleInputChange}
+						onBlur={handleBlur}
+						placeholder='0.00'
+					/>
+					<div className={s.error_container}>
+						{errors.starting_amount && <div>{errors.starting_amount}</div>}
+					</div>
+				</div>
+				<div>
+					<JButton jstyle='primary' type='submit' loading={isSubmitting}>
+						Create Account
+					</JButton>
+					<div className={s.error_container}>
+						{errors.general && <div>{errors.general}</div>}
+					</div>
+				</div>
+			</form>
+		</div>
+	)
+}
---
+++ app/p/accounts/AccountManager/NewAccountForm/insertAccount.ts
@@ -0,0 +1,24 @@
+'use client'
+import { createClient } from '@/utils/supabase/client'
+
+export async function insertAccount(name: string, starting_amount: string) {
+	const supabase = createClient()
+	const {
+		data: { user },
+		error: userError,
+	} = await supabase.auth.getUser()
+
+	if (userError) {
+		return { error: `User error: ${userError}` }
+	}
+
+	const { data, error } = await supabase
+		.from('accounts')
+		.insert([{ name: name, starting_amount: starting_amount, user_id: user!.id }])
+
+	if (error) {
+		return { error: `Data error: ${error}` }
+	} else {
+		return {}
+	}
+}
