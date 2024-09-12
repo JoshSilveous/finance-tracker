7a49bb39a12838153711a94a0b720c0142a63d21
Joshua Silveous <JoshSilveous@gmail.com>
2024-09-12 16:21:26 -0400

    added formatting for large numbers- to be tweaked

--- components/JForm/JNumberAccounting/JNumberAccounting.tsx
+++ components/JForm/JNumberAccounting/JNumberAccounting.tsx
@@ -1,33 +1,56 @@
-import {
-	ChangeEvent,
-	ChangeEventHandler,
-	InputHTMLAttributes,
-	useEffect,
-	useRef,
-	useState,
-} from 'react'
+import { ChangeEvent, InputHTMLAttributes, useRef } from 'react'
 import s from './JNumberAccounting.module.scss'
 
 interface JNumberAccountingProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export default function JNumberAccounting(props: JNumberAccountingProps) {
 	const inputRef = useRef<HTMLInputElement>(null)
-	const handleBlur: ChangeEventHandler<HTMLInputElement> = (e) => {
-		const oldVal = e.target.value
-		const newVal = parseFloat(oldVal).toFixed(2)
-		console.log('oldVal:', oldVal, typeof oldVal)
-		console.log('newVal:', newVal, typeof newVal)
-		e.target.value = newVal
+	function handleBlur(e: ChangeEvent<HTMLInputElement>) {
+		const unroundedVal = e.target.value
+		const roundedVal = parseFloat(unroundedVal).toFixed(2)
+
+		e.target.dataset.rawValue = roundedVal
+
+		const formattedVal = addCommasToNumber(roundedVal)
+		console.log('roundedVal', roundedVal, 'formattedVal', formattedVal)
+
+		e.target.type = 'text'
+		e.target.value = formattedVal
 	}
 
-	useEffect(() => {
-		inputRef.current!.value = parseFloat(inputRef.current!.value).toFixed(2)
-	}, [])
+	function addCommasToNumber(numberString: string) {
+		// Split the string into the whole number and decimal parts
+		let [wholePart, decimalPart] = numberString.split('.')
+
+		// Use a regular expression to add commas to the whole number part
+		wholePart = wholePart.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
+		// Reassemble the whole number and decimal parts (if present)
+		return decimalPart ? `${wholePart}.${decimalPart}` : wholePart
+	}
+
+	function handleFocus(e: ChangeEvent<HTMLInputElement>) {
+		const rawValue = e.target.dataset.rawValue
+		console.log('focus rawValue', rawValue)
+
+		e.target.type = 'number'
+		if (rawValue !== '' && rawValue !== undefined) {
+			e.target.value = rawValue
+		}
+	}
 
 	return (
 		<div className={s.main}>
 			<span className={s.dollar_symbol}>$</span>
-			<input ref={inputRef} {...props} type='number' step={0.01} onBlur={handleBlur} />
+			<input
+				ref={inputRef}
+				{...props}
+				type='text'
+				step={0.01}
+				onBlur={handleBlur}
+				onFocus={handleFocus}
+				data-raw-value={props.value}
+			/>
 		</div>
 	)
 }
