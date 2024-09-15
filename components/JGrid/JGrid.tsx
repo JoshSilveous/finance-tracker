e7f2943fe0af74dba4e7af24b18db93c0b83540a
Joshua Silveous <JoshSilveous@gmail.com>
2024-09-15 13:33:24 -0400

    updated syntax on JGrid

--- components/JGrid/JGrid.tsx
+++ components/JGrid/JGrid.tsx
@@ -23,84 +23,19 @@ export function JGrid({
 	const [columnWidths, setColumnWidths] = useState(defaultColumnWidths)
 	const [isResizing, setIsResizing] = useState(false)
 
-	const Headers = (
-		<div className={styles.row}>
-			{headers.map((itemCell, index) => {
-				const isRightColumn = index === headers.length - 1
-				return (
-					<div
-						key={index}
-						className={`${styles.cell} ${styles.header}`}
-						style={{
-							gridColumn: `${index + 1} / ${index + 2}`,
-							borderRightWidth: isRightColumn && noOuterBorders ? '0px' : '',
-						}}
-					>
-						{itemCell}
-					</div>
-				)
-			})}
-		</div>
-	)
-	const MeasurementSelectors = headers.map((_item, index) => {
-		function handleMouseDown(e: React.MouseEvent) {
-			setIsResizing(true)
-			;(e.target as HTMLDivElement).classList.add(styles.resizing)
-
-			const startX = e.screenX
-			const startWidth = (e.target as HTMLDivElement).parentElement!.clientWidth
-
-			function handleMouseMove(e: MouseEvent) {
-				const curX = e.screenX
-				const diffX = curX - startX
-
-				setColumnWidths((prev) => {
-					const newArr = [...prev]
-					newArr[index] =
-						Math.max(minColumnWidth ? minColumnWidth : 50, startWidth + diffX) + 'px'
-					return newArr
-				})
-
-				window.addEventListener('mouseup', handleMouseUp)
-			}
-
-			function handleMouseUp() {
-				;(e.target as HTMLDivElement).classList.remove(styles.resizing)
-				setIsResizing(false)
-
-				window.removeEventListener('mousemove', handleMouseMove)
-				window.removeEventListener('mouseup', handleMouseUp)
-			}
-
-			window.addEventListener('mousemove', handleMouseMove)
-			window.addEventListener('mouseup', handleMouseUp)
-		}
-
+	function Headers() {
 		return (
-			<div
-				className={styles.measurer}
-				key={index}
-				style={{ gridColumn: `${index + 1} / ${index + 2}` }}
-			>
-				<div className={styles.grabber} onMouseDown={handleMouseDown}></div>
-			</div>
-		)
-	})
-
-	const Content = content.map((itemRow, itemRowIndex) => {
-		return (
-			<div className={styles.row} key={itemRowIndex}>
-				{itemRow.map((itemCell, itemCellIndex) => {
-					const isBottomRow = itemRowIndex === content.length - 1
-					const isRightColumn = itemCellIndex === itemRow.length - 1
+			<div className={styles.row}>
+				{headers.map((itemCell, index) => {
+					const isRightColumn = index === headers.length - 1
 					return (
 						<div
-							className={styles.cell}
-							key={itemCellIndex}
+							key={index}
+							className={`${styles.cell} ${styles.header}`}
 							style={{
-								gridColumn: `${itemCellIndex + 1} / ${itemCellIndex + 2}`,
-								borderBottomWidth: isBottomRow && noOuterBorders ? '0px' : '',
-								borderRightWidth: isRightColumn && noOuterBorders ? '0px' : '',
+								gridColumn: `${index + 1} / ${index + 2}`,
+								borderRightWidth:
+									isRightColumn && noOuterBorders ? '0px' : '',
 							}}
 						>
 							{itemCell}
@@ -109,7 +44,87 @@ export function JGrid({
 				})}
 			</div>
 		)
-	})
+	}
+
+	function MeasurementSelectors() {
+		return headers.map((_item, index) => {
+			function handleMouseDown(e: React.MouseEvent) {
+				setIsResizing(true)
+				;(e.target as HTMLDivElement).classList.add(styles.resizing)
+
+				const startX = e.screenX
+				const startWidth = (e.target as HTMLDivElement).parentElement!.clientWidth
+
+				function handleMouseMove(e: MouseEvent) {
+					const curX = e.screenX
+					const diffX = curX - startX
+
+					setColumnWidths((prev) => {
+						const newArr = [...prev]
+						newArr[index] =
+							Math.max(
+								minColumnWidth ? minColumnWidth : 50,
+								startWidth + diffX
+							) + 'px'
+						return newArr
+					})
+
+					window.addEventListener('mouseup', handleMouseUp)
+				}
+
+				function handleMouseUp() {
+					;(e.target as HTMLDivElement).classList.remove(styles.resizing)
+					setIsResizing(false)
+
+					window.removeEventListener('mousemove', handleMouseMove)
+					window.removeEventListener('mouseup', handleMouseUp)
+				}
+
+				window.addEventListener('mousemove', handleMouseMove)
+				window.addEventListener('mouseup', handleMouseUp)
+			}
+
+			return (
+				<div
+					className={styles.measurer}
+					key={index}
+					style={{ gridColumn: `${index + 1} / ${index + 2}` }}
+				>
+					<div className={styles.grabber} onMouseDown={handleMouseDown}></div>
+				</div>
+			)
+		})
+	}
+
+	function Content() {
+		return content.map((itemRow, itemRowIndex) => {
+			return (
+				<div className={styles.row} key={itemRowIndex}>
+					{itemRow.map((itemCell, itemCellIndex) => {
+						const isBottomRow = itemRowIndex === content.length - 1
+						const isRightColumn = itemCellIndex === itemRow.length - 1
+						return (
+							<div
+								className={styles.cell}
+								key={itemCellIndex}
+								style={{
+									gridColumn: `${itemCellIndex + 1} / ${
+										itemCellIndex + 2
+									}`,
+									borderBottomWidth:
+										isBottomRow && noOuterBorders ? '0px' : '',
+									borderRightWidth:
+										isRightColumn && noOuterBorders ? '0px' : '',
+								}}
+							>
+								{itemCell}
+							</div>
+						)
+					})}
+				</div>
+			)
+		})
+	}
 	return (
 		<div
 			className={`${styles.container} ${className ? className : ''}`}
@@ -124,8 +139,8 @@ export function JGrid({
 					borderWidth: noOuterBorders ? '0px' : '',
 				}}
 			>
-				{Headers}
-				{MeasurementSelectors} {Content}
+				<Headers />
+				<MeasurementSelectors /> <Content />
 			</div>
 		</div>
 	)
