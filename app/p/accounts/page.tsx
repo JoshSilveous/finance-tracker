fd9a0a9ac618fb510cbaf1efa439cbbacf3f1f24
Joshua Silveous <JoshSilveous@gmail.com>
2024-09-15 17:29:57 -0400

    removed excess code

--- app/p/accounts/page.tsx
+++ app/p/accounts/page.tsx
@@ -1,6 +1,4 @@
 import { AccountManager } from './AccountManager/AccountManager'
-import { insertAccount } from './insertAccount'
-import { createClient } from '@/utils/supabase/server'
 import s from './page.module.scss'
 
 export default async function Accounts() {
