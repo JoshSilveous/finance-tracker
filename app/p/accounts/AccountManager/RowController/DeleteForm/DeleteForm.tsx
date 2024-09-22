122fb53c2bd2056f895d0b3e620eb2bdee043f99
Joshua Silveous <JoshSilveous@gmail.com>
2024-09-22 15:49:54 -0400

    fleshed out Delete Account Form

--- app/p/accounts/AccountManager/RowController/DeleteForm/DeleteForm.tsx
+++ app/p/accounts/AccountManager/RowController/DeleteForm/DeleteForm.tsx
@@ -1,6 +1,8 @@
 import { JRadio } from '@/components/JForm/JRadio/JRadio'
 import s from './DeleteForm.module.scss'
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
+import { JDropdown, JDropdownTypes } from '@/components/JForm/JDropdown/JDropdown'
+import { fetchData } from '../../func'
 
 interface DeleteFormProps {
 	account_name: string
@@ -10,11 +12,32 @@ interface DeleteFormProps {
 type DeleteMethods = 'delete' | 'set_null' | 'replace'
 export function DeleteForm({ account_name, account_id, afterDelete }: DeleteFormProps) {
 	const [deleteMethod, setDeleteMethod] = useState<DeleteMethods>()
+	const [otherAccounts, setOtherAccounts] = useState<{ name: string; id: string }[]>()
+	const [accountsAreLoaded, setAccountsAreLoaded] = useState(false)
+
+	async function fetchAccounts() {
+		const data = await fetchData()
+		console.log('loaded:', data)
+		setOtherAccounts(
+			data
+				.filter((thisData) => thisData.id !== account_id)
+				.map((item) => ({ name: item.name, id: item.id }))
+		)
+		setAccountsAreLoaded(true)
+	}
+
 	function handleRadioChange(e: ChangeEvent<HTMLInputElement>) {
 		const selectedMethod = e.target.id as DeleteMethods
 		setDeleteMethod(selectedMethod)
+		if (selectedMethod === 'replace' && !accountsAreLoaded) {
+			console.log('loading accounts')
+			fetchAccounts()
+		}
 	}
-	console.log('deleteMethod', deleteMethod)
+	const handleDropdownChange: JDropdownTypes.ChangeEventHandler = (e) => {
+		console.log('deleteform level', e.target.value)
+	}
+	console.log('accountsAreLoaded', accountsAreLoaded, 'otherAccounts', otherAccounts)
 	return (
 		<div className={s.main}>
 			<h1>Delete "{account_name}"</h1>
@@ -34,6 +57,23 @@ export function DeleteForm({ account_name, account_id, afterDelete }: DeleteForm
 					account.
 				</JRadio>
 			</div>
+			{deleteMethod === 'replace' && (
+				<div className={s.replace_dropdown}>
+					<p>Choose an account to replace "{account_name}" with:</p>
+					<JDropdown
+						options={
+							accountsAreLoaded
+								? otherAccounts!.map((item) => ({
+										name: item.name,
+										value: item.id,
+								  }))
+								: []
+						}
+						onChange={handleDropdownChange}
+						loading={!accountsAreLoaded}
+					/>
+				</div>
+			)}
 		</div>
 	)
 }
