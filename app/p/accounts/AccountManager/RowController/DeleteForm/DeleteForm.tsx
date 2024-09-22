5823fa865766186a16195861cecce57c4df2b4fa
Joshua Silveous <JoshSilveous@gmail.com>
2024-09-22 14:19:48 -0400

    added radio options to delete form

--- app/p/accounts/AccountManager/RowController/DeleteForm/DeleteForm.tsx
+++ app/p/accounts/AccountManager/RowController/DeleteForm/DeleteForm.tsx
@@ -1,11 +1,20 @@
+import { JRadio } from '@/components/JForm/JRadio/JRadio'
 import s from './DeleteForm.module.scss'
+import { ChangeEvent, useState } from 'react'
 
 interface DeleteFormProps {
 	account_name: string
 	account_id: string
 	afterDelete: () => void
 }
+type DeleteMethods = 'delete' | 'set_null' | 'replace'
 export function DeleteForm({ account_name, account_id, afterDelete }: DeleteFormProps) {
+	const [deleteMethod, setDeleteMethod] = useState<DeleteMethods>()
+	function handleRadioChange(e: ChangeEvent<HTMLInputElement>) {
+		const selectedMethod = e.target.id as DeleteMethods
+		setDeleteMethod(selectedMethod)
+	}
+	console.log('deleteMethod', deleteMethod)
 	return (
 		<div className={s.main}>
 			<h1>Delete "{account_name}"</h1>
@@ -13,24 +22,17 @@ export function DeleteForm({ account_name, account_id, afterDelete }: DeleteForm
 				There are x transactions associated with this account. How would you like to
 				handle those transactions?
 			</p>
-			<div>
-				<input type='radio' id='huey' name='drone' value='huey' />
-				<label htmlFor='huey'>Delete the transactions</label>
-			</div>
-
-			<div>
-				<input type='radio' id='dewey' name='drone' value='dewey' />
-				<label htmlFor='dewey'>
-					Keep the transactions, and set their account attribute to Empty .
-				</label>
-			</div>
-
-			<div>
-				<input type='radio' id='louie' name='drone' value='louie' />
-				<label htmlFor='louie'>
+			<div className={s.radio_options}>
+				<JRadio id='delete' name='handle_delete' onChange={handleRadioChange}>
+					Delete the transactions
+				</JRadio>
+				<JRadio id='set_null' name='handle_delete' onChange={handleRadioChange}>
+					Keep the transactions, and set their account attribute to Empty.
+				</JRadio>
+				<JRadio id='replace' name='handle_delete' onChange={handleRadioChange}>
 					Keep the transactions, and change their account attribute to a different
 					account.
-				</label>
+				</JRadio>
 			</div>
 		</div>
 	)
