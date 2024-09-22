4b38de1533ef941268ce155636554ee7bd60a823
Joshua Silveous <JoshSilveous@gmail.com>
2024-09-22 14:15:50 -0400

    added JRadio input type

---
+++ components/JForm/JRadio/JRadio.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+import s from './JRadio.module.scss'
+interface JRadioProps extends React.HTMLProps<HTMLInputElement> {
+	id: string
+	children?: ReactNode
+}
+
+export function JRadio(props: JRadioProps) {
+	return (
+		<label className={`${s.main} ${props.className ? props.className : ''}`}>
+			<input type='radio' {...props} className={''} children={undefined} />
+			<div className={s.custom_dot} />
+			<span className={s.children}>{props.children}</span>
+		</label>
+	)
+}
