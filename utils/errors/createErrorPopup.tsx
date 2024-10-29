6d4ceb55bef6e261f29e8a02e5eb87e93ad48b70
Joshua Silveous <JoshSilveous@gmail.com>
2024-10-29 18:00:41 -0400

    added new error handling prompt function- WIP

--- utils/errors/createErrorPopup.tsx
+++ utils/errors/createErrorPopup.tsx
@@ -13,3 +13,20 @@ export function createErrorPopup(error: string) {
 		'error'
 	).trigger()
 }
+
+function promptError(errorMsg: string, instructions: string) {
+	createPopup(
+		<div>
+			<h2>An unexpected error has occured.</h2>
+			<h4>Error: "{error}"</h4>
+			<p>
+				Try refreshing the page. Eventually I'll put in better error handling
+				instructions here :)
+			</p>
+		</div>,
+		'error',
+		() => {
+			location.reload()
+		}
+	).trigger()
+}
