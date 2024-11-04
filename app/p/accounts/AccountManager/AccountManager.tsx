d64ad959c12359d81584f7b00da82f3781852d57
Joshua Silveous <JoshSilveous@gmail.com>
2024-11-04 11:52:50 -0500

    fleshed out redo system

--- app/p/accounts/AccountManager/AccountManager.tsx
+++ app/p/accounts/AccountManager/AccountManager.tsx
@@ -20,6 +20,9 @@ import {
 	saveChanges,
 	handleInputChange,
 	handleInputBlur,
+	undoMostRecentAction,
+	HistoryItem,
+	redoMostRecentAction,
 } from './func'
 import { RowController } from './RowController/RowController'
 
@@ -30,19 +33,6 @@ export interface Change {
 		starting_amount?: string
 	}
 }
-interface HistoryItemReorder {
-	action: 'reorder'
-	oldIndex: number
-	newIndex: number
-}
-interface HistoryItemValueChange {
-	action: 'value_change'
-	account_id: string
-	key: keyof Account.Bare
-	oldVal: string
-	newVal: string
-}
-export type HistoryItem = HistoryItemReorder | HistoryItemValueChange
 
 export function AccountManager() {
 	const bgLoad = useBgLoad()
@@ -58,7 +48,10 @@ export function AccountManager() {
 	const gridRowRefs = useRef<HTMLDivElement[]>([])
 
 	/**
-	 * use below refs in event listeners to ensure you're pulling the latest data, as event handlers don't always get updated on re-renders
+	 * use below refs in event listeners to ensure you're pulling the latest data.
+	 * event listeners are only defined once, so if you reference the state directly it will
+	 * pull old data from when the function was defined.
+	 * by referencing a ref instead, you get updated data every time.
 	 */
 	const pendingChangesRef = useRef<Change[]>(pendingChanges)
 	const currentSortOrderRef = useRef<Account.ID[]>(currentSortOrder)
@@ -68,7 +61,6 @@ export function AccountManager() {
 	const redoHistoryStackRef = useRef<HistoryItem[]>(redoHistoryStack)
 	useEffect(() => {
 		pendingChangesRef.current = pendingChanges
-		console.log('pendingChanges changed!', pendingChangesRef.current)
 	}, [pendingChanges])
 	useEffect(() => {
 		currentSortOrderRef.current = currentSortOrder
@@ -81,7 +73,6 @@ export function AccountManager() {
 	}, [isLoading])
 	useEffect(() => {
 		undoHistoryStackRef.current = undoHistoryStack
-		console.log('undoHistoryStack', undoHistoryStack)
 	}, [undoHistoryStack])
 	useEffect(() => {
 		redoHistoryStackRef.current = redoHistoryStack
@@ -172,125 +163,33 @@ export function AccountManager() {
 				}
 
 				// CTRL+Z to undo
-				if (e.ctrlKey && !e.shiftKey && e.key.toUpperCase() === 'Z') {
+				else if (e.ctrlKey && !e.shiftKey && e.key.toUpperCase() === 'Z') {
 					e.preventDefault()
 					if (undoHistoryStackRef.current.length !== 0) {
-						const mostRecentAction = undoHistoryStackRef.current.at(-1)!
-						// need to update pendingChanges when ran
-						if (mostRecentAction.action === 'reorder') {
-							const { newIndex, oldIndex } = mostRecentAction
-							setCurrentSortOrder((prev) => {
-								const newArr = [...prev!]
-								const [item] = newArr.splice(newIndex, 1)
-								newArr.splice(oldIndex, 0, item)
-								return newArr
-							})
-						} else {
-							const { account_id, key, oldVal, newVal } = mostRecentAction
-
-							const node = document.querySelector(
-								`[data-id="${account_id}"][data-key="${key}"]`
-							) as HTMLInputElement
-
-							const thisPendingChangeIndex =
-								pendingChangesRef.current.findIndex(
-									(item) => item.account_id === account_id
-								)
-							if (thisPendingChangeIndex !== -1) {
-								// if there is a pendingChange for the entry we are undo'ing
-								const thisPendingChange =
-									pendingChangesRef.current[thisPendingChangeIndex]
-
-								let defaultVal = node.dataset['default'] as string
-								let returningToDefault = false
-								if (
-									(!isNaN(parseInt(defaultVal)) &&
-										!isNaN(parseInt(oldVal)) &&
-										parseInt(defaultVal) === parseInt(oldVal)) ||
-									defaultVal === oldVal
-								) {
-									returningToDefault = true
-								}
-
-								if (returningToDefault) {
-									// if we are returning to the default value
-									if (
-										(key === 'name' &&
-											thisPendingChange.new.starting_amount ===
-												undefined) ||
-										(key === 'starting_amount' &&
-											thisPendingChange.new.name === undefined)
-									) {
-										// if other keys don't exist on pendingChange, remove change
-										setPendingChanges((prev) =>
-											prev.filter(
-												(_, index) =>
-													index !== thisPendingChangeIndex
-											)
-										)
-									} else {
-										// if other keys DO exist on pendingChange, set key to undefined
-										setPendingChanges((prev) => {
-											const newArr = structuredClone(prev)
-											newArr[thisPendingChangeIndex].new[key] =
-												undefined
-											return newArr
-										})
-									}
-								} else {
-									// set pendingChange to reflect the old value
-									setPendingChanges((prev) => {
-										const newArr = structuredClone(prev)
-										newArr[thisPendingChangeIndex].new[key] = oldVal
-										return newArr
-									})
-								}
-							} else {
-								// add a pendingChange
-								setPendingChanges((prev) => [
-									...prev,
-									{ account_id: account_id, new: { [key]: oldVal } },
-								])
-							}
-							node.focus()
-						}
-						setRedoHistoryStack((prev) => [...prev, mostRecentAction])
-						setUndoHistoryStack((prev) => prev.slice(0, -1))
+						undoMostRecentAction(
+							undoHistoryStackRef,
+							setCurrentSortOrder,
+							pendingChangesRef,
+							setPendingChanges,
+							setUndoHistoryStack,
+							setRedoHistoryStack
+						)
 					}
 					return
 				}
-				// NEXT: ADD REDO LOGIC
+
 				// CTRL+SHIFT+Z to redo
-				if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === 'Z') {
-					console.log('pendingChanges:', pendingChanges)
+				else if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === 'Z') {
 					e.preventDefault()
 					if (redoHistoryStackRef.current.length !== 0) {
-						const mostRecentUndoAction = redoHistoryStackRef.current.at(-1)!
-						if (mostRecentUndoAction?.action === 'reorder') {
-							setCurrentSortOrder((prev) => {
-								const newArr = [...prev!]
-								const [item] = newArr.splice(
-									mostRecentUndoAction.oldIndex,
-									1
-								)
-								newArr.splice(mostRecentUndoAction.newIndex, 0, item)
-								return newArr
-							})
-						} else {
-							const node = document.querySelector(
-								`[data-id="${mostRecentUndoAction.account_id}"][data-key="${mostRecentUndoAction.key}"]`
-							) as HTMLInputElement
-							console.log(
-								'SETTING THIS NODE VAL TO',
-								mostRecentUndoAction.newVal,
-								node
-							)
-							node.value = mostRecentUndoAction.newVal
-							node.focus()
-							node.blur()
-						}
-						setUndoHistoryStack((prev) => [...prev, mostRecentUndoAction])
-						setRedoHistoryStack((prev) => prev.slice(0, -1))
+						redoMostRecentAction(
+							redoHistoryStackRef,
+							setCurrentSortOrder,
+							pendingChangesRef,
+							setPendingChanges,
+							setUndoHistoryStack,
+							setRedoHistoryStack
+						)
 					}
 					return
 				}
