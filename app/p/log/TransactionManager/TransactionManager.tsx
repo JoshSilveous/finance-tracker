bbd5638f50ee642b4be152e62595f5b8ec68befa
Joshua Silveous <JoshSilveous@gmail.com>
2024-11-29 17:05:10 -0500

    abstracted row generation logic

--- app/p/log/TransactionManager/TransactionManager.tsx
+++ app/p/log/TransactionManager/TransactionManager.tsx
@@ -15,6 +15,7 @@ import { JGrid, JGridTypes } from '@/components/JGrid/JGrid'
 import { JInput, JNumberAccounting } from '@/components/JForm'
 import { JDropdown, JDropdownTypes } from '@/components/JForm/JDropdown/JDropdown'
 import { JDatePicker } from '@/components/JForm/JDatePicker/JDatePicker'
+import { genMultiRow, genSingleRow } from './func/row_gen/row_gen'
 interface LoadState {
 	loading: boolean
 	message: string
@@ -71,33 +72,37 @@ export function TransactionManager() {
 		if (categories === null) {
 			return []
 		} else {
-			return categories.map((cat) => {
+			const options = categories.map((cat) => {
 				return {
 					name: cat.name,
 					value: cat.id,
 				}
 			})
+			options.unshift({ name: 'None', value: 'none' })
+			return options
 		}
 	}, [categories])
 	const dropdownOptionsAccount: JDropdownTypes.Option[] = useMemo(() => {
 		if (accounts === null) {
 			return []
 		} else {
-			return accounts.map((act) => {
+			const options = accounts.map((act) => {
 				return {
 					name: act.name,
 					value: act.id,
 				}
 			})
+			options.unshift({ name: 'None', value: 'none' })
+			return options
 		}
 	}, [accounts])
 
 	const headers: JGridTypes.Header[] = [
-		{ content: <div className={s.header_container}>Date</div>, defaultWidth: 100 },
-		{ content: <div className={s.header_container}>Name</div>, defaultWidth: 300 },
+		{ content: <div className={s.header_container}>Date</div>, defaultWidth: 125 },
+		{ content: <div className={s.header_container}>Name</div>, defaultWidth: 250 },
 		{ content: <div className={s.header_container}>Amount</div>, defaultWidth: 100 },
-		{ content: <div className={s.header_container}>Category</div>, defaultWidth: 100 },
-		{ content: <div className={s.header_container}>Account</div>, defaultWidth: 100 },
+		{ content: <div className={s.header_container}>Category</div>, defaultWidth: 150 },
+		{ content: <div className={s.header_container}>Account</div>, defaultWidth: 150 },
 	]
 
 	let grid: ReactNode
@@ -114,225 +119,23 @@ export function TransactionManager() {
 			const cells: JGridTypes.Props['cells'] = []
 			data.forEach((transaction) => {
 				if (transaction.items.length === 1) {
-					const transactionItem = transaction.items[0]
-					cells.push([
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.single_item} ${s.first_col}`}
-								>
-									<JDatePicker defaultValue={transaction.date} />
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.single_item} ${s.mid_col}`}
-								>
-									<JInput value={transaction.name} />
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.single_item} ${s.mid_col}`}
-								>
-									<JNumberAccounting value={transactionItem.amount} />
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.single_item} ${s.mid_col}`}
-								>
-									<JDropdown
-										options={dropdownOptionsCategory}
-										defaultValue={
-											transactionItem.category_id !== null
-												? transactionItem.category_id
-												: undefined
-										}
-									/>
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.single_item} ${s.last_col}`}
-								>
-									<JDropdown
-										options={dropdownOptionsAccount}
-										defaultValue={
-											transactionItem.account_id !== null
-												? transactionItem.account_id
-												: undefined
-										}
-									/>
-								</div>
-							),
-						},
-					])
+					cells.push(
+						genSingleRow(
+							transaction,
+							dropdownOptionsCategory,
+							dropdownOptionsAccount
+						)
+					)
 				} else {
-					let sum = 0
-					const nextRows = transaction.items.map((item, itemIndex) => {
-						sum += item.amount
-
-						const isLastRow = itemIndex === transaction.items.length - 1
-
-						return [
-							{
-								content: (
-									<div
-										className={`${s.data_container} ${s.multi_item} ${
-											s.first_col
-										} ${isLastRow ? s.last_row : s.mid_row}`}
-									>
-										<JDatePicker
-											defaultValue={transaction.date}
-											disabled
-											minimalStyle
-										/>
-									</div>
-								),
-							},
-							{
-								content: (
-									<div
-										className={`${s.data_container} ${s.multi_item} ${
-											s.mid_col
-										} ${isLastRow ? s.last_row : s.mid_row}`}
-									>
-										<JInput value={item.name} />
-									</div>
-								),
-							},
-							{
-								content: (
-									<div
-										className={`${s.data_container} ${s.multi_item} ${
-											s.mid_col
-										} ${isLastRow ? s.last_row : s.mid_row}`}
-									>
-										<JNumberAccounting value={item.amount} />
-									</div>
-								),
-							},
-							{
-								content: (
-									<div
-										className={`${s.data_container} ${s.multi_item} ${
-											s.mid_col
-										} ${isLastRow ? s.last_row : s.mid_row}`}
-									>
-										<JDropdown
-											options={dropdownOptionsCategory}
-											defaultValue={
-												item.category_id !== null
-													? item.category_id
-													: undefined
-											}
-										/>
-									</div>
-								),
-							},
-							{
-								content: (
-									<div
-										className={`${s.data_container} ${s.multi_item} ${
-											s.last_col
-										} ${isLastRow ? s.last_row : s.mid_row}`}
-									>
-										<JDropdown
-											options={dropdownOptionsAccount}
-											defaultValue={
-												item.account_id !== null
-													? item.account_id
-													: undefined
-											}
-										/>
-									</div>
-								),
-							},
-						]
-					})
-
-					let categoryList = ''
-					let accountList = ''
-					transaction.items.forEach((item) => {
-						if (item.category_id !== null) {
-							const categoryName = categories.find(
-								(cat) => cat.id === item.category_id
-							)!.name
-							if (categoryList === '') {
-								categoryList += categoryName
-							} else {
-								categoryList += ', ' + categoryName
-							}
-						}
-						if (item.account_id !== null) {
-							const accountName = accounts.find(
-								(act) => act.id === item.account_id
-							)!.name
-							if (accountList === '') {
-								accountList += accountName
-							} else {
-								accountList += ', ' + accountName
-							}
-						}
-					})
-
-					const firstRow = [
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.multi_item} ${s.first_row} ${s.first_col}`}
-								>
-									<JDatePicker defaultValue={transaction.date} />
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${s.first_row}`}
-								>
-									<JInput value={transaction.name} />
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${s.first_row}`}
-								>
-									<JNumberAccounting value={sum} disabled minimalStyle />
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${s.first_row}`}
-								>
-									<JInput value={categoryList} disabled minimalStyle />
-								</div>
-							),
-						},
-						{
-							content: (
-								<div
-									className={`${s.data_container} ${s.multi_item} ${s.last_col} ${s.first_row}`}
-								>
-									<JInput value={accountList} disabled minimalStyle />
-								</div>
-							),
-						},
-					]
-					cells.push(firstRow, ...nextRows)
+					cells.push(
+						...genMultiRow(
+							transaction,
+							categories,
+							accounts,
+							dropdownOptionsCategory,
+							dropdownOptionsAccount
+						)
+					)
 				}
 			})
 			const gridConfig: JGridTypes.Props = {
---
+++ app/p/log/TransactionManager/func/row_gen/row_gen.tsx
@@ -0,0 +1,217 @@
+import { JInput, JNumberAccounting } from '@/components/JForm'
+import { JDatePicker } from '@/components/JForm/JDatePicker/JDatePicker'
+import { JDropdown, JDropdownTypes } from '@/components/JForm/JDropdown/JDropdown'
+import { FetchedAccount, FetchedCategory, FetchedTransaction } from '@/database'
+import s from './row_gen.module.scss'
+
+export function genSingleRow(
+	transaction: FetchedTransaction,
+	dropdownOptionsCategory: JDropdownTypes.Option[],
+	dropdownOptionsAccount: JDropdownTypes.Option[]
+) {
+	const transactionItem = transaction.items[0]
+	return [
+		{
+			content: (
+				<div className={`${s.data_container} ${s.single_item} ${s.first_col}`}>
+					<JDatePicker defaultValue={transaction.date} />
+				</div>
+			),
+		},
+		{
+			content: (
+				<div className={`${s.data_container} ${s.single_item} ${s.mid_col}`}>
+					<JInput value={transaction.name} />
+				</div>
+			),
+		},
+		{
+			content: (
+				<div className={`${s.data_container} ${s.single_item} ${s.mid_col}`}>
+					<JNumberAccounting value={transactionItem.amount} />
+				</div>
+			),
+		},
+		{
+			content: (
+				<div className={`${s.data_container} ${s.single_item} ${s.mid_col}`}>
+					<JDropdown
+						options={dropdownOptionsCategory}
+						defaultValue={
+							transactionItem.category_id !== null
+								? transactionItem.category_id
+								: undefined
+						}
+					/>
+				</div>
+			),
+		},
+		{
+			content: (
+				<div className={`${s.data_container} ${s.single_item} ${s.last_col}`}>
+					<JDropdown
+						options={dropdownOptionsAccount}
+						defaultValue={
+							transactionItem.account_id !== null
+								? transactionItem.account_id
+								: undefined
+						}
+					/>
+				</div>
+			),
+		},
+	]
+}
+
+export function genMultiRow(
+	transaction: FetchedTransaction,
+	categories: FetchedCategory[],
+	accounts: FetchedAccount[],
+	dropdownOptionsCategory: JDropdownTypes.Option[],
+	dropdownOptionsAccount: JDropdownTypes.Option[]
+) {
+	let sum = 0
+	const nextRows = transaction.items.map((item, itemIndex) => {
+		sum += item.amount
+
+		const isLastRow = itemIndex === transaction.items.length - 1
+
+		return [
+			{
+				content: (
+					<div
+						className={`${s.data_container} ${s.multi_item} ${s.first_col} ${
+							isLastRow ? s.last_row : s.mid_row
+						}`}
+					>
+						<JDatePicker defaultValue={transaction.date} disabled minimalStyle />
+					</div>
+				),
+			},
+			{
+				content: (
+					<div
+						className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${
+							isLastRow ? s.last_row : s.mid_row
+						}`}
+					>
+						<JInput value={item.name} />
+					</div>
+				),
+			},
+			{
+				content: (
+					<div
+						className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${
+							isLastRow ? s.last_row : s.mid_row
+						}`}
+					>
+						<JNumberAccounting value={item.amount} />
+					</div>
+				),
+			},
+			{
+				content: (
+					<div
+						className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${
+							isLastRow ? s.last_row : s.mid_row
+						}`}
+					>
+						<JDropdown
+							options={dropdownOptionsCategory}
+							defaultValue={
+								item.category_id !== null ? item.category_id : undefined
+							}
+						/>
+					</div>
+				),
+			},
+			{
+				content: (
+					<div
+						className={`${s.data_container} ${s.multi_item} ${s.last_col} ${
+							isLastRow ? s.last_row : s.mid_row
+						}`}
+					>
+						<JDropdown
+							options={dropdownOptionsAccount}
+							defaultValue={
+								item.account_id !== null ? item.account_id : undefined
+							}
+						/>
+					</div>
+				),
+			},
+		]
+	})
+
+	let categoryList = ''
+	let accountList = ''
+	transaction.items.forEach((item) => {
+		if (item.category_id !== null) {
+			const categoryName = categories.find((cat) => cat.id === item.category_id)!.name
+			if (categoryList === '') {
+				categoryList += categoryName
+			} else {
+				categoryList += ', ' + categoryName
+			}
+		}
+		if (item.account_id !== null) {
+			const accountName = accounts.find((act) => act.id === item.account_id)!.name
+			if (accountList === '') {
+				accountList += accountName
+			} else {
+				accountList += ', ' + accountName
+			}
+		}
+	})
+
+	const firstRow = [
+		{
+			content: (
+				<div
+					className={`${s.data_container} ${s.multi_item} ${s.first_row} ${s.first_col}`}
+				>
+					<JDatePicker defaultValue={transaction.date} />
+				</div>
+			),
+		},
+		{
+			content: (
+				<div
+					className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${s.first_row}`}
+				>
+					<JInput value={transaction.name} />
+				</div>
+			),
+		},
+		{
+			content: (
+				<div
+					className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${s.first_row}`}
+				>
+					<JNumberAccounting value={sum} disabled minimalStyle />
+				</div>
+			),
+		},
+		{
+			content: (
+				<div
+					className={`${s.data_container} ${s.multi_item} ${s.mid_col} ${s.first_row}`}
+				>
+					<JInput value={categoryList} disabled minimalStyle />
+				</div>
+			),
+		},
+		{
+			content: (
+				<div
+					className={`${s.data_container} ${s.multi_item} ${s.last_col} ${s.first_row}`}
+				>
+					<JInput value={accountList} disabled minimalStyle />
+				</div>
+			),
+		},
+	]
+	return [firstRow, ...nextRows]
+}
