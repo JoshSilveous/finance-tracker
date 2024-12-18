b9c99d306420ba04b30aa3c72105392d80d3377e
Joshua Silveous <JoshSilveous@gmail.com>
2024-12-18 17:55:18 -0500

    worked on new JNumberAccounting system

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -2,37 +2,12 @@
 import { JNumberAccounting } from '@/components/JForm'
 import { useState } from 'react'
 import s from './page.module.scss'
+import { NewJNumberAccounting } from '@/components/JForm/NewJNumberAccounting/NewJNumberAccounting'
 
 export default function Dev() {
-	const itemcount = 1000
-
-	const items: JSX.Element[] = []
-	for (let i = 0; i < itemcount; i++) {
-		items.push(<div className={s.grid_item}>Content {i + 1}</div>)
-	}
 	return (
-		<div className={s.scrollable_container}>
-			<div>
-				<div className={s.grid_container}>
-					<div>
-						<div className={`${s.grid_item}  ${s.sticky}`}>
-							Measurement Selector 1
-						</div>
-					</div>
-					<div>
-						<div className={`${s.grid_item}  ${s.sticky}`}>
-							Measurement Selector 2
-						</div>
-					</div>
-					<div>
-						<div className={`${s.grid_item}  ${s.sticky}`}>
-							Measurement Selector 3
-						</div>
-					</div>
-
-					{items}
-				</div>
-			</div>
+		<div className={s.main}>
+			<NewJNumberAccounting />
 		</div>
 	)
 }
