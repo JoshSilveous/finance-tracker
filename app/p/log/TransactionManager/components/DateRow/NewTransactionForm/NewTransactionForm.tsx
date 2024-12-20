7984208ab81c3b46f0c39931295a8c6f19a18b22
Joshua Silveous <JoshSilveous@gmail.com>
2024-12-19 21:21:49 -0500

    worked on NewTransactionForm

--- app/p/log/TransactionManager/components/DateRow/NewTransactionForm/NewTransactionForm.tsx
+++ app/p/log/TransactionManager/components/DateRow/NewTransactionForm/NewTransactionForm.tsx
@@ -1,21 +1,18 @@
 import { JInput, JNumberAccounting } from '@/components/JForm'
 import s from './NewTransactionForm.module.scss'
 import { JDatePicker } from '@/components/JForm/JDatePicker/JDatePicker'
-import { JRadio } from '@/components/JForm/JRadio/JRadio'
-import { useEffect, useState } from 'react'
-import { FetchedAccount, FetchedCategory } from '@/database'
-import { JGridTypes } from '@/components/JGrid/JGrid'
-import { JDropdown, JDropdownTypes } from '@/components/JForm/JDropdown/JDropdown'
+import { useState } from 'react'
+import { JDropdown } from '@/components/JForm/JDropdown/JDropdown'
 import { DropdownOptions } from '../../../TransactionManager'
 
 interface TransactionFormData {
-	name: string
-	date: string
-	items: {
-		name: string
-		amount: string
-		category_id: string
-		account_id: string
+	name?: string
+	date?: string
+	items?: {
+		name?: string
+		amount?: string
+		category_id?: string
+		account_id?: string
 	}[]
 }
 
@@ -28,18 +25,22 @@ export function NewTransactionForm({
 	defaultDate,
 	dropdownOptions,
 }: NewTransactionFormProps) {
+	const [formData, setFormData] = useState<TransactionFormData>({})
 	const [isMultiItems, setIsMultiItems] = useState(false)
+
 	return (
 		<div className={s.main}>
 			<h2>Create New Transaction</h2>
 			<form>
-				<div>
-					<label htmlFor='transaction_name'>Name:</label>
-					<JInput />
-				</div>
-				<div>
-					<label htmlFor='transaction_name'>Date:</label>
-					<JDatePicker defaultValue={defaultDate} />
+				<div className={s.split_col}>
+					<div>
+						<label htmlFor='transaction_name'>Name:</label>
+						<JInput />
+					</div>
+					<div>
+						<label htmlFor='transaction_name'>Date:</label>
+						<JDatePicker defaultValue={defaultDate} />
+					</div>
 				</div>
 				<div>
 					<label htmlFor='multiple_items'>Multiple Items?</label>
@@ -50,19 +51,27 @@ export function NewTransactionForm({
 						onChange={(e) => setIsMultiItems(e.target.checked)}
 					/>
 				</div>
-				<div className={s.single_item}>
-					<div>
-						<label htmlFor='item_amount'>Amount:</label>
-						<JNumberAccounting value={0} />
-					</div>
-					<div>
-						<label htmlFor='item_category'>Category:</label>
-						<JDropdown options={dropdownOptions.category} />
-					</div>
-					<div>
-						<label htmlFor='item_account'>Account:</label>
-						<JDropdown options={dropdownOptions.account} />
-					</div>
+				<div className={s.item_container}>
+					{isMultiItems ? (
+						formData!.items!.map((item) => {
+							return <div>hi</div>
+						})
+					) : (
+						<div className={s.item}>
+							<div>
+								<label htmlFor='item_amount'>Amount:</label>
+								<JNumberAccounting value={0} />
+							</div>
+							<div>
+								<label htmlFor='item_category'>Category:</label>
+								<JDropdown options={dropdownOptions.category} />
+							</div>
+							<div>
+								<label htmlFor='item_account'>Account:</label>
+								<JDropdown options={dropdownOptions.account} />
+							</div>
+						</div>
+					)}
 				</div>
 			</form>
 		</div>
