7806f4822f4e4d72b1b97a334c3e9b3c1b044962
Joshua Silveous <JoshSilveous@gmail.com>
2024-12-22 14:56:14 -0500

    added JCheckbox

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -3,25 +3,12 @@ import { JNumberAccounting } from '@/components/JForm'
 import { useState } from 'react'
 import s from './page.module.scss'
 import { insertTransactionAndItems } from '@/database'
+import { JCheckbox } from '@/components/JForm/JCheckbox/JCheckbox'
 
 export default function Dev() {
-	function submit() {
-		insertTransactionAndItems({
-			name: 'new transaction',
-			date: '2024-12-22',
-			items: [
-				{ amount: '1253.32', name: 'test item', category_id: '', account_id: '' },
-				{ amount: '1243.32', name: 'test item', category_id: '', account_id: '' },
-				{ amount: '1223.32', name: 'test item', category_id: '', account_id: '' },
-				{ amount: '1213.32', name: 'test item', category_id: '', account_id: '' },
-				{ amount: '1523.32', name: 'test item', category_id: '', account_id: '' },
-				{ amount: '1123.32', name: 'test item', category_id: '', account_id: '' },
-			],
-		})
-	}
 	return (
 		<div className={s.main}>
-			<button onClick={submit}>Test!</button>
+			<JCheckbox />
 		</div>
 	)
 }
---
+++ components/JForm/JCheckbox/JCheckbox.tsx
@@ -0,0 +1,47 @@
+import { DetailedHTMLProps, InputHTMLAttributes, useCallback, useState } from 'react'
+import s from './JCheckbox.module.scss'
+import { default as CheckmarkIcon } from '@/public/checkmark.svg'
+
+interface JCheckboxProps
+	extends Omit<
+		DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+		'type'
+	> {
+	/**
+	 * background color is required to be explicitly defined because the checkmark's `color` attribute cannot inherit `background-color`.
+	 */
+	bgColor: string
+}
+export function JCheckbox({
+	bgColor,
+	style,
+	onChange: propsOnChange,
+	...rest
+}: JCheckboxProps) {
+	const [isChecked, setIsChecked] = useState(
+		rest.checked !== undefined ? rest.checked : false
+	)
+
+	const onChange: React.ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+		setIsChecked(e.target.checked)
+		if (propsOnChange !== undefined) {
+			propsOnChange(e)
+		}
+	}, [])
+	return (
+		<div className={s.main}>
+			<input
+				type='checkbox'
+				style={{ backgroundColor: bgColor, ...style }}
+				onChange={onChange}
+				{...rest}
+			/>
+			<div
+				style={{ color: bgColor }}
+				className={`${s.checkmark_container} ${isChecked ? s.checked : ''}`}
+			>
+				<CheckmarkIcon />
+			</div>
+		</div>
+	)
+}
