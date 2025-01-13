2fb5e7f6f4fd9dc10a4c2cf7b0d0af6544b853cb
Joshua Silveous <JoshSilveous@gmail.com>
2025-01-13 18:13:08 -0500

    added greeting

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,11 +1,16 @@
 import { SignOutButton } from './SignOutButton'
 import s from './NavBar.module.scss'
 import { NavLinks } from './NavLinks/NavLinks'
+import { createClient } from '@/database/supabase/server'
 
 export default async function NavBar() {
+	const supabase = createClient()
+	const { data } = await supabase.auth.getUser()
+	const name = data.user?.user_metadata.name || data.user?.email
 	return (
 		<div className={s.container}>
 			<NavLinks />
+			<div className={s.greeting}>Hello, {name}!</div>
 			<div className={s.sign_out_container}>
 				<SignOutButton />
 			</div>
--- components/NavBar/NavLinks/NavLinks.tsx
+++ components/NavBar/NavLinks/NavLinks.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef } from 'react'
 import s from './NavLinks.module.scss'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
