911f61af1437e31165e41227b70a07f5025547b9
Joshua Silveous <JoshSilveous@gmail.com>
2025-01-14 09:40:24 -0500

    fixed signup with github not storing session token

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -86,7 +86,7 @@ export function LoginForm() {
 		const { data, error } = await supabase.auth.signInWithOAuth({
 			provider: 'github',
 			options: {
-				redirectTo: `${window.location.origin}/p/dashboard`,
+				redirectTo: `${window.location.origin}/auth/callback`,
 			},
 		})
 		if (error) {
