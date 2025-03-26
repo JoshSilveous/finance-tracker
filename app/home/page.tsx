fee78ef71cdcf1be2bc5e6a5f439286d409b488c
Joshua Silveous <JoshSilveous@gmail.com>
2025-03-26 19:37:25 -0400

    worked on landing page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from 'next/navigation'
 import s from './page.module.scss'
 import { createClient } from '@/database/supabase/server'
 import { default as LBLogo } from '@/public/lb_logo.svg'
+import Image from 'next/image'
 
 import Link from 'next/link'
 import { DemoButton } from './DemoButton/DemoButton'
@@ -14,13 +15,22 @@ export default async function Home() {
 
 	return (
 		<div className={s.main}>
+			<div className={s.top_bar}>
+				<div className={s.logo_container}>
+					<LBLogo />
+					<p>LedgerBoard</p>
+				</div>
+				<div className={s.action_container}>
+					{userAlreadyLoggedIn ? (
+						<Link href='/p/dashboard'>Dashboard</Link>
+					) : (
+						<Link href='/login'>Login</Link>
+					)}
+				</div>
+			</div>
 			<div className={s.content_container}>
 				<div className={s.content}>
 					<div className={s.first_tagline_container}>
-						<div className={s.logo_container}>
-							<LBLogo />
-							<p>LedgerBoard</p>
-						</div>
 						<h1>Your finances. Your way.</h1>
 						<p className={s.some_text}>
 							A manual-first finance app for clarity, control, and
@@ -35,6 +45,37 @@ export default async function Home() {
 									<DemoButton className={s.demo_button} />
 								</>
 							)}
+						</div>
+					</div>
+					<div className={s.image_container}>
+						<Image
+							src='/images/landing_1.png'
+							width={1920}
+							height={1080}
+							alt='REPLACEME'
+						/>
+					</div>
+					<div className={s.more_details_container}>
+						<div className={s.detail}>
+							<p className={s.bold}>Manual-first. Meaningful always.</p>
+							<p className={s.reg}>
+								Log transactions, categorize your way, and track spending
+								with full control and intent.
+							</p>
+						</div>
+						<div className={s.detail}>
+							<p className={s.bold}>Built for customization.</p>
+							<p className={s.reg}>
+								Create a personalized dashboard to match your unique
+								budgeting and financial tracking style.
+							</p>
+						</div>
+						<div className={s.detail}>
+							<p className={s.bold}>No ads. No gimmicks. Just clarity.</p>
+							<p className={s.reg}>
+								No tracking, ads, or upsells—just a clear, distraction-free
+								view of your finances.
+							</p>
 						</div>
 					</div>
 				</div>
